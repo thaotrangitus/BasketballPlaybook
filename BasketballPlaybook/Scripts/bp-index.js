58322902acff0b8f3640a8726b1b27349e17913f
Trang N. T. Ngo <trang.itus@gmail.com>
2018-05-13 10:26:38 +0700

    first commit

---
+++ BasketballPlaybook/Scripts/bp-index.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    var data = [
+        { Id: 1, Name: "Ball Handling" },
+        { Id: 2, Name: "Passing" },
+        { Id: 3, Name: "Shooting" },
+        { Id: 4, Name: "Rebounding" },
+        { Id: 5, Name: "Transition" },
+        { Id: 6, Name: "Defense" },
+        { Id: 7, Name: "Team offense" },
+        { Id: 8, Name: "Team Defense" },
+    ];
+
+    var viewModel = {
+        //data
+        tags: ko.observableArray(data),
+        tagToAdd: ko.observable(""),
+
+        //behavior
+        addTag: function () {
+            this.tags.push({ Name: this.tagToAdd() });
+            this.tagToAdd("");
+        }
+    };
+
+    ko.applyBindings(viewModel);
+
+    $(".tag-delete").click(function () {
+        var itemToRemove = ko.dataFor(this);//this is refering to the Dome element that we're databindings  to.
+        viewModel.tags.remove(itemToRemove);
+    });
+});
