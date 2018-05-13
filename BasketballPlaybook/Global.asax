1903323c2d3d578ff9e7e6805f43ea73bb9bd7b9
Trang N. T. Ngo <trang.itus@gmail.com>
2018-05-13 09:45:18 +0700

    Add project files.

---
+++ BasketballPlaybook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BasketballPlaybook.MvcApplication" Language="C#" %>
---
+++ BasketballPlaybook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BasketballPlaybook.Startup))]
+namespace BasketballPlaybook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
